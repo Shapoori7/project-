a4f5a9307ffd9468044ee6963867d23d129a0b75
nejadipour <nejadipour@gmail.com>
2021-12-17 13:29:48 +0330

    User model initial implementation

---
+++ src/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+}
