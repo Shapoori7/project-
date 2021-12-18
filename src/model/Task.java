fdfad59ee1db037e67be3763e1cc4e051a0b79a2
nejadipour <nejadipour@gmail.com>
2021-12-18 18:06:59 +0330

    task model other parts completed

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,8 +1,11 @@
 package model;
 
+import java.util.*;
 import java.util.UUID;
 import java.util.Date;
 import java.util.ArrayList;
+import User;
+import TaskPriority;
 
 public class Task {
     private UUID id;
@@ -16,6 +19,29 @@ public class Task {
 
     public Task() {
         this.id = UUID.randomUUID();
+        this.assignedUsers = new ArrayList<>();
+        this.created = new Date();
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'|'hh:mm");
+        return "Task{" +
+                "Title='" + title + '\'' +
+                ", Description='" + description + '\'' +
+                ", Priority=" + priority +
+                ", Date and time of creation='" + dateFormat.format(created) + '\'' +
+                ", Date and time of deadline=" + dateFormat.format(deadline) +
+                ", Assigned users=\n" + usersToString() +
+                '}';
+    }
+
+    private String usersToString() {
+        String usres = "";
+        for (User user: assignedUsers) {
+            users.join(user.getUsername());
+        }
+        return users;
     }
 
 }
